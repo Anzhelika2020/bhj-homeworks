3920f4d47352b3a4ad9e1bb88ab5a0197b250d2e
Anzhelika Popova <anjelikasp@mail.ru>
2021-08-29 01:44:44 +0700

    task 2.3 rework

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,49 +1,58 @@
 "use strict";
 
-const sliders = document.querySelectorAll(".slider__item");
+const sliders = Array.from(document.querySelectorAll(".slider__item"));
 
 const sliderArrowPrev = document.querySelector (".slider__arrow_prev");
 
 const sliderArrowNext = document.querySelector(".slider__arrow_next");
 
+const sliderDots = Array.from(document.querySelectorAll(".slider__dot"));
+
+sliderDots[0].className = "slider__dot slider__dot_active";
+
+
+//управление с помощъю стрелок
 sliderArrowNext.onclick = sliderSwitch;
 
 sliderArrowPrev.onclick = sliderSwitch;
 
-
 function sliderSwitch () {
-  for (let i = 0; i < sliders.length; i++) {
-    if (sliders[i].className.includes("active")) {
-      sliders[i].className = "slider__item";
+  let i = sliders.findIndex(element => element.className.includes("active"));
+  
+  sliders[i].className = "slider__item";
 
-      if (this.className.includes("next")) {
-        i = CalcNextSlider(i);
-      } else if (this.className.includes("prev")) {
-        i = CalcPrevSlider(i);
-      };
+  sliderDots[i].className = "slider__dot";
+  
+  this.className.includes("next") ? i = сalcNextSlider(i) : i = сalcPrevSlider(i);
 
-      sliders[i].className = "slider__item slider__item_active";
-    };
-  };
-}
+  sliders[i].className = "slider__item slider__item_active";
 
-function CalcNextSlider (i) {
-  if (i === sliders.length - 1) {
-    i = 0;
-  } else {
-    i++;
-  };
+  sliderDots[i].className = "slider__dot slider__dot_active";
+};
+
+function сalcNextSlider (i) {
+  i === sliders.length - 1 ? i = 0 : i++;
 
   return i;
-}
+};
 
-function CalcPrevSlider (i) {
-  if (i === 0) {
-    i = sliders.length - 1;
-  } else {
-    i--;
-  };
-
+function сalcPrevSlider (i) {
+  i === 0 ? i = sliders.length - 1 : i--;
+  
   return i;
-}
+};
+
+
+//управление с помощъю точек
+for (let i = 0; i < sliderDots.length; i++) {
+  sliderDots[i].onclick = function() {
+    document.querySelector(".slider__item_active").className = "slider__item";
+
+    document.querySelector(".slider__dot_active").className = "slider__dot";
+    
+    sliders[i].className = "slider__item slider__item_active";
+
+    sliderDots[i].className = "slider__dot slider__dot_active";
+  };
+};
 
