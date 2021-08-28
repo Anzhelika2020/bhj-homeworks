e1529dc7708e2804d20efd60baa2a36eb5364c0c
Anzhelika Popova <anjelikasp@mail.ru>
2021-08-29 00:33:41 +0700

    task 2.2 rework

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -2,27 +2,27 @@
 
 //1 показ окна при запуске
 
-let modalMain = document.getElementById("modal_main");
+const modalMain = document.getElementById("modal_main");
 
-let modalSuccess = document.getElementById("modal_success");
+const modalSuccess = document.getElementById("modal_success");
 
 modalMain.className = "modal modal_active";
 
 
 //2 закрытие окна разными кнопками
-let closeElms = document.getElementsByClassName("modal__close");
+const closeElms = document.getElementsByClassName("modal__close");
 
 for (let i = 0; i < closeElms.length; i++) {
   closeElms[i].onclick = function () {
-    modalMain.className = "modal";
-    modalSuccess.className = "modal";
+    this.closest(".modal_active").className = "modal";
   };
 };
 
 
 //3 открытие окна успеха
-let showSuccessBtn = modalMain.querySelector(".show-success");
+const showSuccessBtn = modalMain.querySelector(".show-success");
 
 showSuccessBtn.onclick = function () {
+  modalMain.className = "modal";
   modalSuccess.className = "modal modal_active";
 };
