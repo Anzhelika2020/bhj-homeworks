72ae24b84f422dfeaa194f433e1dea5888cbc615
Anzhelika Popova <anjelikasp@mail.ru>
2021-08-31 17:22:28 +0700

    task 3.1 rework

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,26 +1,38 @@
 "use strict";
 
-const btnValue = document.querySelector(".dropdown__value");
-
-const menuList = document.querySelector(".dropdown__list");
+const btnsValueEls = Array.from(document.querySelectorAll(".dropdown__value"));
 
 const menuItems = Array.from(document.querySelectorAll(".dropdown__item"));
 
 const menuLinks = Array.from(document.querySelectorAll(".dropdown__link"));
 
-btnValue.onclick = function () {
-  menuList.className.includes("list_active") ? 
-  menuList.className = "dropdown__list" : 
-  menuList.className = "dropdown__list dropdown__list_active";
+
+for (let i = 0; i < btnsValueEls.length; i++) {
+  btnsValueEls[i].onclick = function () {
+    const activeMenuList = document.querySelector(".dropdown__list_active");
+
+    const thisMenuList = this.parentElement.querySelector(".dropdown__list");
+
+    if (thisMenuList.className.includes("list_active")) {
+      activeMenuList.className = "dropdown__list";
+
+    } else if (activeMenuList) {
+      activeMenuList.className = "dropdown__list";
+
+      thisMenuList.className = "dropdown__list dropdown__list_active";
+
+    } else {
+      thisMenuList.className = "dropdown__list dropdown__list_active";
+    };
+  };
 };
 
 for (let i = 0; i < menuItems.length; i++) {
   menuItems[i].onclick = function () {
-    menuList.className = "dropdown__list";
+    this.parentElement.className = "dropdown__list";
 
-    btnValue.textContent = menuLinks[i].textContent;
+    this.closest(".dropdown").querySelector(".dropdown__value").textContent = menuLinks[i].textContent;
 
     return false;
-  }
-
+  };
 };
