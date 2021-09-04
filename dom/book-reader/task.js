1f10c05b87c0c985408f8faebc82e5c5b5f0729f
Anzhelika Popova <anjelikasp@mail.ru>
2021-09-04 17:18:54 +0700

    task 2 rework

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -4,30 +4,83 @@ const bookElm = document.getElementById("book");
 
 const controlFS = document.querySelector(".book__control_font-size");
 
-const fontSizeElms = Array.from(controlFS.querySelectorAll(".font-size"));
+const controlColor = document.querySelector(".book__control_color");
 
-fontSizeElms.forEach((element) => {
-  element.onclick = function () {
-    if (this.className.includes("big")) {
-      bookElm.className = "book book_fs-big";
+const controlBackground = document.querySelector(".book__control_background");
 
-    } else if (this.className.includes("small")) {
-      bookElm.className = "book book_fs-small";
 
-    } else {
-      bookElm.className = "book";
+const fontSizeBtns = Array.from(controlFS.querySelectorAll(".font-size"));
+
+const colorBtns = Array.from(controlColor.querySelectorAll(".color"));
+
+const backgroundBtns = Array.from(controlBackground.querySelectorAll(".color"));
+
+
+function changeBtn (button, classActiveBtn) {
+  if (button.className.includes(classActiveBtn)) {
+    return false;
+
+  } else {
+    button.closest(".book__control").querySelector(`.${classActiveBtn}`).classList.remove(classActiveBtn);
+
+    button.classList.add(classActiveBtn);
+  };
+};
+
+
+fontSizeBtns.forEach((button) => {
+  button.onclick = function () {
+    bookElm.classList.remove("book_fs-big", "book_fs-small");
+
+    if (this.dataset.size ==="big") {
+      bookElm.classList.add("book_fs-big");
+
+    } else if (this.dataset.size === "small") {
+      bookElm.classList.add("book_fs-small");
     };
 
-    if (this.className.includes("size_active")) {
-      return false;
-    } else {
-      this.closest(".book__control").querySelector(".font-size_active").classList.remove("font-size_active");
+    changeBtn(button, "font-size_active");
 
-      this.classList.add("font-size_active");
+    return false;
+  };
+});
 
-      bookElm.classList.add
+colorBtns.forEach((button) => {
+  button.onclick = function () {
+    bookElm.classList.remove("book_color-gray", "book_color-whitesmoke", "book_color-black");
 
-      return false;
+    if (this.dataset.textColor === "gray") {
+      bookElm.classList.add("book_color-gray");
+
+    } else if (this.dataset.textColor === "whitesmoke") {
+      bookElm.classList.add("book_color-whitesmoke");
+
+    } else if (this.dataset.textColor === "black") {;
+      bookElm.classList.add("book_color-black");
     };
+
+    changeBtn(button, "color_active");
+
+    return false;
+  };
+});
+
+backgroundBtns.forEach((button) => {
+  button.onclick = function () {
+    bookElm.classList.remove("book_bg-gray", "book_bg-black", "book_bg-white");
+
+    if (this.dataset.bgColor === "gray") {
+      bookElm.classList.add("book_bg-gray");
+
+    } else if (this.dataset.bgColor === "black") {
+      bookElm.classList.add("book_bg-black");
+
+    } else if (this.dataset.bgColor === "white") {
+      bookElm.classList.add("book_bg-white");
+    };
+
+    changeBtn(button, "color_active");
+
+    return false;
   };
 });
