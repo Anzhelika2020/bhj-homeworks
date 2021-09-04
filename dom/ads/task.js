b7f166878545848b0f58c7bcecb0428d6ee2bf87
Anzhelika Popova <anjelikasp@mail.ru>
2021-09-04 18:34:39 +0700

    task 1 rework

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -9,9 +9,16 @@ function changeAds () {
   cardsElms.forEach((card) => {
     const rotatorCases = Array.from(card.querySelectorAll(".rotator__case"));
 
+    rotatorCases.forEach((e) => {
+      e.style.color = e.dataset.color;
+    });
+
     let i = rotatorCases.indexOf(card.querySelector(".rotator__case_active"));
 
-    setInterval(changeCase, 1000);
+    let speed = rotatorCases[i].dataset.speed;
+
+    let idTimeout = setTimeout(changeCase, speed);
+ 
 
     function changeCase () {
       rotatorCases[i].classList.remove("rotator__case_active");
@@ -19,6 +26,12 @@ function changeAds () {
       i === rotatorCases.length - 1 ? i = 0 : i++;
 
       rotatorCases[i].classList.add("rotator__case_active");
+
+      clearTimeout(idTimeout);
+
+      speed = rotatorCases[i].dataset.speed;
+
+      idTimeout = setTimeout(changeCase, speed);
     };
   });
 };
