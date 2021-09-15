989794f08baf6a6a03958ea679e1625434213187
Anzhelika Popova <anjelikasp@mail.ru>
2021-09-15 13:22:52 +0700

    task 3 redone

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,29 +1,36 @@
 "use strict";
 
-const hasTooltips = document.querySelectorAll(".has-tooltip");
+const aHasTooltips = document.querySelectorAll(".has-tooltip");
 
 let tooltipElm = document.createElement("div");
 
 tooltipElm.className = "tooltip";
 
-hasTooltips.forEach((elm) => {
+aHasTooltips.forEach((elm) => {
   elm.onclick = function() {
-    elm.insertAdjacentElement("afterEnd", tooltipElm);
 
-    tooltipElm.textContent = elm.title;
+    if (tooltipElm.textContent === elm.title) {
+      tooltipElm.textContent = "";
+      tooltipElm.className = "tooltip";
 
-    tooltipElm.className = "tooltip_active";
-    
-    tooltipElm.style.position = "absolute";
-    tooltipElm.style.background = "black";
-    tooltipElm.style.color = "white";
-    tooltipElm.style.borderRadius = "4px";
-    tooltipElm.style.left = `${elm.getBoundingClientRect().left}px`;
+    } else {
+      elm.insertAdjacentElement("afterEnd", tooltipElm);
 
-    //топ нет смысла задавать, он и так на нужном нам уровне
-    //tooltipElm.style.top = `${elm.getBoundingClientRect().bottom}px`;
+      tooltipElm.textContent = elm.title;
+
+      tooltipElm.className = "tooltip_active";
+      
+      tooltipElm.style.position = "absolute";
+      tooltipElm.style.background = "black";
+      tooltipElm.style.color = "white";
+      tooltipElm.style.borderRadius = "4px";
+      tooltipElm.style.left = `${elm.getBoundingClientRect().left}px`;
+
+      //топ нет смысла задавать, он и так на нужном нам уровне
+      //tooltipElm.style.top = `${elm.getBoundingClientRect().bottom}px`;
+    };
 
     return false;
-  }
-
+  };
 });
+
